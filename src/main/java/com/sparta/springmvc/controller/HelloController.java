161b60a920087bae6d5d243e7351557bb5020ead
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-06-23 16:05:29 +0900

    Controller 이해하기

---
+++ src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.sparta.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!!!";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "Get Method 요청";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "Post Method 요청";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "Put Method 요청";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "Delete Method 요청";
+    }
+
+}
